3b99f6efe78cdf563a6f2bc064fff1e9a17677eb
김희석 <kim1140129@gmail.com>
2024-04-11 21:00:22 +0900

    feat: PerspectiveCamera 구현

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css"
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/**
+ * Perspective : 원근법이 적용되는 3D 화면에서 주로 사용됩니다
+ * Orthographic : 직각 투영으로 원근법이 없는 2D 화면에서 주로 사용됩니다
+ * */
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth/window.innerHeight,
+    0.1,
+    100
+);
+camera.position.z = 5;
+camera.position.y = 1;
 
-setupCounter(document.querySelector('#counter'))
+const geometry =  new THREE.BoxGeometry(1,1,1,)
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix()
+    renderer.render(scene,camera);
+})
+renderer.render(scene,camera);
